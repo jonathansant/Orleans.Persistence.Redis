7a9868c3cf71ce68de7bb86db095001fecd394e1
djgrech <39998811+djgrech@users.noreply.github.com>
2023-02-03 12:18:06 +0100

    updating DeflateSerializer (#19)
    Co-authored-by: Derek Grech <derek.grech@river.tech>

--- Orleans.Persistence.Redis/Serialization/DeflateSerializer.cs
+++ Orleans.Persistence.Redis/Serialization/DeflateSerializer.cs
@@ -3,37 +3,41 @@ using System;
 using System.IO;
 using System.IO.Compression;
 
-namespace Orleans.Persistence.Redis.Serialization;
-public class DeflateSerializer : Serialization.OrleansSerializer
+namespace Orleans.Persistence.Redis.Serialization
 {
-	public DeflateSerializer(SerializationManager serializationManager) : base(serializationManager)
+	public class DeflateSerializer : Serialization.OrleansSerializer
 	{
-	}
-
-	public override object Deserialize(byte[] serializedData, Type type)
-		=> base.Deserialize(Decompress(serializedData), type);
-
-	public override byte[] Serialize(object raw, Type type)
-		=> Compress(base.Serialize(raw, type));
-
-	private static byte[] Decompress(byte[] bytes)
-	{
-		using var memoryStream = new MemoryStream(bytes);
-		using var outputStream = new MemoryStream();
-		using (var decompressStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+		public DeflateSerializer(SerializationManager serializationManager) : base(serializationManager)
 		{
-			decompressStream.CopyTo(outputStream);
 		}
-		return outputStream.ToArray();
-	}
 
-	private static byte[] Compress(byte[] bytes)
-	{
-		using var memoryStream = new MemoryStream();
-		using (var compressStream = new DeflateStream(memoryStream, CompressionMode.Compress))
+		public override object Deserialize(byte[] serializedData, Type type)
+			=> base.Deserialize(Decompress(serializedData), type);
+
+		public override byte[] Serialize(object raw, Type type)
+			=> Compress(base.Serialize(raw, type));
+
+		private static byte[] Decompress(byte[] bytes)
 		{
-			compressStream.Write(bytes, 0, bytes.Length);
+			using var memoryStream = new MemoryStream(bytes);
+			using var outputStream = new MemoryStream();
+			using (var decompressStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+			{
+				decompressStream.CopyTo(outputStream);
+			}
+
+			return outputStream.ToArray();
 		}
-		return memoryStream.ToArray();
+
+		private static byte[] Compress(byte[] bytes)
+		{
+			using var memoryStream = new MemoryStream();
+			using (var compressStream = new DeflateStream(memoryStream, CompressionMode.Compress))
+			{
+				compressStream.Write(bytes, 0, bytes.Length);
+			}
+
+			return memoryStream.ToArray();
+		}
 	}
 }
